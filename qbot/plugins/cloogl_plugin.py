a1d4575d72b9499a9b20792ba031003b9e9db63e
Mart Lubbers <mart@martlubbers.net>
2017-10-11 19:54:08 +0200

    cloo.gl: Add the apikey as a configurable option

--- qbot/plugins/cloogl_plugin.py
+++ qbot/plugins/cloogl_plugin.py
@@ -12,6 +12,11 @@ logger = logging.getLogger(__name__)
 class ClooglPlugin(BasePlugin):
     '''ClooglPlugin shortens the given url using the cloo.gl shortening service
 
+    Configurable values:
+        [cloogl]
+        # Should contain the api key
+        apikey = a
+
     Examples:
         user | ~cloogl example.com
         qbot | http://cloo.gl/MzQy
@@ -21,6 +26,7 @@ class ClooglPlugin(BasePlugin):
 
     def __init__(self, **kwargs):
         logger.info('Creating cloo.gl plugin instance')
+        self.apikey = kwargs['cloogl']['apikey']
         self.regex_mappings = {
                 r'~cloogl (.*)': self.cloogl,
         }
@@ -28,10 +34,11 @@ class ClooglPlugin(BasePlugin):
     def cloogl(self, user, match):
         url = match.group(1)
         encurl = quote(url)
+        apikey = quote(self.apikey)
         logger.debug(f'Shortening: {url}')
         response = urlopen(
             "https://cloo.gl",
-            f"type=regular&token=a&url={encurl}".encode('utf-8'))
+            f"type=regular&token={apikey}&url={encurl}".encode('utf-8'))
         short = response.read().decode('utf-8')
         logger.debug(f'response: {short}')
         return short.strip()
