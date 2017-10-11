d33a2d485d976f20af5a1900fc490d52da03d8f8
Mart Lubbers <mart@martlubbers.net>
2017-10-11 19:17:53 +0200

    Add cloo.gl plugin

---
+++ qbot/plugins/cloogl_plugin.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from . import BasePlugin
+from urllib.request import urlopen
+from urllib.parse import quote
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class ClooglPlugin(BasePlugin):
+    '''ClooglPlugin shortens the given url using the cloo.gl shortening service
+
+    Examples:
+        user | ~cloogl example.com
+        qbot | http://cloo.gl/MzQy
+
+    Matches all messages starting with ~cloogl
+    '''
+
+    def __init__(self, **kwargs):
+        logger.info('Creating cloo.gl plugin instance')
+        self.regex_mappings = {
+                r'~cloogl (.*)': self.cloogl,
+        }
+
+    def cloogl(self, user, match):
+        url = match.group(1)
+        encurl = quote(url)
+        logger.debug(f'Shortening: {url}')
+        response = urlopen(
+            "https://cloo.gl",
+            f"type=regular&token=a&url={encurl}".encode('utf-8'))
+        short = response.read().decode('utf-8')
+        logger.debug(f'response: {short}')
+        return short.strip()
